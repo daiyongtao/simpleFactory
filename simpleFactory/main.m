2bd6b8db3cbbfd637ce8547977047d02adab404f
daiyongtao <18811508093@163.com>
2022-05-14 16:24:15 +0800

    first commit

---
+++ simpleFactory/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  simpleFactory
+//
+//  Created by 花果山松鼠 on 2022/5/14.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
